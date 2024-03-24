f1e403d834e52773a8f7a9ced843dd3c0e8bb755
ibrahim97zak <121764716+ibrahim97zak@users.noreply.github.com>
2024-03-24 11:35:31 +0200

    path fix

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,17 +2,23 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth/auth.rouets.js"
 import messageRoutes from "./routes/message/message.rouets.js"
 import usersRoutes from "./routes/users/user.routes.js"
 import connectToDB from "./DB/connectDb.js";
 import { app, server } from "./socket/socket.js";
+import { fileURLToPath } from "url";
 
 const PORT = process.env.PORT || 5000;
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename)
 
 const __dirname=path.resolve()
 
 dotenv.config();
+
 app.use(express.json()); // to parse the incoming requests with JSON payloads(from req.body)
 app.use(cookieParser())//to handle cookies and be able to access them
 
@@ -20,13 +26,16 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", usersRoutes);
 
-app.use(express.static(path.join(__dirname,"/FrontEnd/dist")));
+const staticPath = path.join(__dirname, '../FrontEnd/dist');
+app.use(express.static(staticPath));
 
+//render front/client at any path
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname,"FrontEnd","dist", "index.html"))
+    res.sendFile(path.join(__dirname,'../FrontEnd/dist/index.html'))
 })
 
 server.listen(PORT, () => {
     connectToDB();
     console.log(`Server is running on ${PORT}`)
+    console.log("hi",__dirname)
 });
